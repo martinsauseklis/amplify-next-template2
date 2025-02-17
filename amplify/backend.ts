a8e534f6b761379c42561918022780fecf60f185
Mārtiņš Mārtiņš <martinamauseklim@gmail.com>
2025-02-17 20:02:46 +0200

    testing AGAIN

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
+import { sayHello } from './functions/say-hello/resource.js';
 
 defineBackend({
   auth,
   data,
+  sayHello
 });
